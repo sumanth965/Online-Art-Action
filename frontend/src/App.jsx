08d6eedfa8230d22c26dc0c21cc87f2c1ca6286b
sumanth965 <sumanthpoojary965@gmail.com>
2025-10-27 20:32:49 +0530

    Added 3 folders to pages along with one file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Dummyy from "./dummyy/dummyy";
-import ArtistDashboard from "./pages/ArtistDashboard";
+import ArtistDashboard from "./pages/artist/ArtistDashboard";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/a" element={<Dummyy />} />
-        <Route path="/" element={<ArtistDashboard />} />
+        <Route path="/" element={<Dummyy />} />
+        <Route path="/a" element={<ArtistDashboard />} />
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/pages/emty.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function emty() {
-  return (
-    <div>emty</div>
-  )
-}
